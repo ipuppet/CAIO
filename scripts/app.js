f7756fea8bc3069924c6c6b8c1515cda921cf32c
子明 <37722993+ipuppet@users.noreply.github.com>
2021-03-01 17:25:26 +0800

    更新框架

--- scripts/app.js
+++ scripts/app.js
@@ -18,19 +18,6 @@ class AppKernel extends Kernel {
         this.actionPath = "/scripts/action"
     }
 
-    uuid() {
-        let s = []
-        const hexDigits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        for (let i = 0; i < 36; i++) {
-            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
-        }
-        s[14] = "4" // bits 12-15 of the time_hi_and_version field to 0010
-        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1) // bits 6-7 of the clock_seq_hi_and_reserved to 01
-        s[8] = s[13] = s[18] = s[23] = "-"
-
-        return s.join("")
-    }
-
     getActions(type) {
         const actions = []
         const fileList = $file.list(this.actionPath)
@@ -95,20 +82,17 @@ class AppKernel extends Kernel {
      */
     initSettingMethods() {
         this.setting.readme = animate => {
-            animate.touchHighlightStart()
+            animate.touchHighlight()
             const content = $file.read("/README.md").string
-            this.UIKit.push({
-                view: [{
+            this.UIKit.pushPageSheet({
+                views: [{
                     type: "markdown",
                     props: { content: content },
                     layout: (make, view) => {
                         make.size.equalTo(view.super)
                     }
                 }],
-                title: $l10n("README"),
-                disappeared: () => {
-                    animate.touchHighlightEnd()
-                }
+                title: $l10n("README")
             })
         }
 
