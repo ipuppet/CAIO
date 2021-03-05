9169b1516150ea5c4ecda5cf44fea5bdd2860f0b
子明 <37722993+ipuppet@users.noreply.github.com>
2021-03-05 21:05:54 +0800

    更新文档和注释

--- scripts/action/clipboard/OpenUrl/main.js
+++ scripts/action/clipboard/OpenUrl/main.js
@@ -4,15 +4,7 @@ class MyAction extends Action {
     openUrl(url) {
         $app.openURL(url)
     }
-
-    /**
-     * 系统会调用 do() 方法
-     * 
-     * 可用数据如下：
-     * this.config 配置文件内容
-     * this.text 当前复制的文本或选中的文本亦或者编辑器内的文本
-     * this.uuid 该文本的 uuid
-     */
+    
     do() {
         const regex = /(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([:0-9])*([\/\w\.\-\?\=\&])*\s?/ig
         const text = this.text ?? ""
