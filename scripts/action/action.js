e159be643eafbd769f6fad1287babd56a3cae23c
子明 <37722993+ipuppet@users.noreply.github.com>
2021-03-05 20:55:13 +0800

    更新数据处理方式

--- scripts/action/action.js
+++ scripts/action/action.js
@@ -2,8 +2,7 @@ class Action {
     constructor(kernel, config, data) {
         this.kernel = kernel
         this.config = config
-        this.text = data.text
-        this.selectedRange = data.selectedRange
+        Object.assign(this, data)
     }
 }
 
