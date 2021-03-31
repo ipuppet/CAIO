33bffd1e14809e172b76cdaf0b2a08d13af2f424
子明 <37722993+ipuppet@users.noreply.github.com>
2021-03-31 18:46:55 +0800

    新增push方法

--- scripts/action/README.md
+++ scripts/action/README.md
@@ -39,4 +39,20 @@ module.exports = MyAction
 - `this.selectedRange` 在编辑器中，当前选中的文本范围 `{location: Number, length: Number}`
 - `this.selectedText` 在编辑器中，当前选中的文本
 
-系统会调用当前 `Action` 的 `do()` 方法
+父类的方法：
+```js
+/**
+ * page sheet
+ * @param {*} args 
+ *  {
+        views: args.views, // 视图数组
+        title: args.title ?? "", // 中间标题
+        navButtons: args.navButtons ?? [], // 右上角的按钮
+        topOffset = args.topOffset ?? true, // 视图是否占满整个page sheet。
+                                            // 若不占满标题栏有模糊效果，但会覆盖一部分视图，需手动调整。
+        done: args.done, // 点击左上角按钮后的回调函数
+        doneText: args.doneText ?? $l10n("DONE") // 左上角文本
+    }
+  */
+push(args): void;
+```
--- scripts/action/action.js
+++ scripts/action/action.js
@@ -4,6 +4,23 @@ class Action {
         this.config = config
         Object.assign(this, data)
     }
+
+    /**
+     * page sheet
+     * @param {*} args 
+     *  {
+            views: args.views, // 视图数组
+            title: args.title ?? "", // 中间标题
+            navButtons: args.navButtons ?? [], // 右上角的按钮
+            topOffset = args.topOffset ?? true, // 视图是否占满整个page sheet。
+                                                // 若不占满标题栏有模糊效果，但会覆盖一部分视图，需手动调整。
+            done: args.done, // 点击左上角按钮后的回调函数
+            doneText: args.doneText ?? $l10n("DONE") // 左上角文本
+        }
+     */
+    push(args) {
+        this.kernel.pushPageSheet(args)
+    }
 }
 
 module.exports = Action
