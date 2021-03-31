ed073ef01dc18ace8d897af0312247007afaf58c
子明 <37722993+ipuppet@users.noreply.github.com>
2021-04-01 00:20:19 +0800

    未检测到链接时给予提示

--- scripts/action/clipboard/OpenUrl/main.js
+++ scripts/action/clipboard/OpenUrl/main.js
@@ -4,20 +4,22 @@ class MyAction extends Action {
     openUrl(url) {
         $app.openURL(url)
     }
-    
+
     do() {
         const regex = /(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([:0-9])*([\/\w\.\-\?\=\&])*\s?/ig
         const text = this.text ?? ""
-        const url = text.match(regex, text)
+        const url = text.match(regex, text) ?? []
         if (url.length > 1) {
             $ui.menu({
                 items: url,
-                handler: (title, idx) => {
-                    this.openUrl(url[idx])
+                handler: (title, index) => {
+                    this.openUrl(url[index])
                 }
             })
-        } else {
+        } else if (url.length === 1) {
             this.openUrl(url[0])
+        } else {
+            $ui.warning("未检测到链接")
         }
     }
 }
