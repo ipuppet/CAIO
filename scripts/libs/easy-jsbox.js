63f97edee7a4ea6394f066ca2c536b00942c68e1
ipuppet <zimingw.ol@outlook.com>
2022-06-12 16:26:53 +0800

    update

--- scripts/libs/easy-jsbox.js
+++ scripts/libs/easy-jsbox.js
@@ -2174,6 +2174,73 @@ class Kernel {
     }
 }
 
+class UILoading {
+    #labelId
+    text = ""
+    interval
+    fullScreen = false
+    #loop = () => { }
+
+    constructor() {
+        this.#labelId = uuid()
+    }
+
+    updateText(text) {
+        $(this.#labelId).text = text
+    }
+
+    setLoop(loop) {
+        if (typeof loop !== "function") {
+            throw "loop must be a function"
+        }
+        this.#loop = loop
+    }
+
+    done() {
+        clearInterval(this.interval)
+    }
+
+    load() {
+        $ui.render({
+            props: {
+                navBarHidden: this.fullScreen
+            },
+            views: [
+                {
+                    type: "spinner",
+                    props: {
+                        loading: true
+                    },
+                    layout: (make, view) => {
+                        make.centerY.equalTo(view.super).offset(-15)
+                        make.width.equalTo(view.super)
+                    }
+                },
+                {
+                    type: "label",
+                    props: {
+                        id: this.#labelId,
+                        align: $align.center,
+                        text: ""
+                    },
+                    layout: (make, view) => {
+                        make.top.equalTo(view.prev.bottom).offset(10)
+                        make.left.right.equalTo(view.super)
+                    }
+                }
+            ],
+            layout: $layout.fill,
+            events: {
+                appeared: () => {
+                    this.interval = setInterval(() => {
+                        this.#loop()
+                    }, 100)
+                }
+            }
+        })
+    }
+}
+
 class FileStorageParameterError extends Error {
     constructor(parameter) {
         super(`Parameter [${parameter}] is required.`)
@@ -3671,6 +3738,7 @@ module.exports = {
     versionCompare,
     compressImage,
     // class
+    View,
     UIKit,
     ViewController,
     Matrix,
@@ -3688,6 +3756,7 @@ module.exports = {
     TabBarHeaderView,
     TabBarController,
     Kernel,
+    UILoading,
     FileStorage,
     Setting
 }
