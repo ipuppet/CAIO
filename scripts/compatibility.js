3de51faeec5c1bd309642b22b9a94681c0456e67
ipuppet <zimingw.ol@outlook.com>
2023-07-07 10:30:03 +0800

    优化结构

--- scripts/compatibility.js
+++ scripts/compatibility.js
@@ -2,187 +2,239 @@
  * @typedef {import("./app").AppKernel} AppKernel
  */
 
-/**
- * 删除文件
- * @param {AppKernel} kernel
- */
-function deleteFiles(kernel, files = []) {
-    files.forEach(file => {
-        if ($file.exists(file)) {
-            kernel.print(`delete file: ${file}`)
-            $file.delete(file)
-        }
-    })
-}
+class Compatibility {
+    files = []
+    databases = []
+    actions = {}
 
-/**
- * 重建表
- * @param {AppKernel} kernel
- */
-function rebuildDatabase(kernel, oldTab, newTab) {
-    const result = kernel.storage.sqlite.query(
-        `SELECT count(*), name FROM sqlite_master WHERE type = "table" AND name = "${oldTab}"`
-    )
-    if (result.error !== null) {
-        throw new Error(`Code [${result.error.code}] ${result.error.domain} ${result.error.localizedDescription}`)
-    }
-    result.result.next()
-    const count = result.result.get(0)
-    result.result.close()
-
-    if (count > 0) {
-        kernel.print(`copy data from old table: ${oldTab}`)
-        kernel.storage.sqlite.update(`INSERT INTO ${newTab} SELECT * FROM ${oldTab}`)
-        kernel.print(`drop table: ${oldTab}`)
-        kernel.storage.sqlite.update(`DROP TABLE ${oldTab}`)
-    }
-}
-
-/**
- * 用户动作
- * @param {AppKernel} kernel
- */
-async function rebuildUserActions(kernel, actions = {}) {
-    const actionPath = `scripts/action`
-    const userActionPath = `${kernel.fileStorage.basePath}/user_action`
-
-    const changeList = []
-    for (let type of Object.keys(actions)) {
-        actions[type].forEach(action => {
-            const config = JSON.parse($file.read(`${actionPath}/${type}/${action}/config.json`).string)
-            changeList.push(config.name)
-        })
+    /**
+     * @param {AppKernel} kernel
+     */
+    constructor(kernel) {
+        this.kernel = kernel
     }
 
-    const alertResult = await $ui.alert({
-        title: $l10n("compatibility.rebuildUserAction.alert.title"),
-        message:
-            $l10n("compatibility.rebuildUserAction.alert.message") +
-            "\n" +
-            JSON.stringify(changeList, null, 2) +
-            "\n" +
-            $l10n("compatibility.rebuildUserAction.alert.message2"),
-        actions: [{ title: $l10n("OK") }, { title: $l10n("CANCEL") }]
-    })
-    if (alertResult.index === 1) {
-        return
-    }
-
-    // 重建用户动作
-    for (let type of Object.keys(actions)) {
-        actions[type].forEach(action => {
-            if ($file.exists(`${userActionPath}/${type}/${action}`)) {
-                kernel.print(`rebuild user action: ${type}/${action}`)
-                $file.copy({
-                    src: `${actionPath}/${type}/${action}/main.js`,
-                    dst: `${userActionPath}/${type}/${action}/main.js`
-                })
+    deleteFiles(files) {
+        files.forEach(file => {
+            if (!this.files.includes(file)) {
+                this.files.push(file)
             }
         })
     }
-    kernel.actionManager.setNeedReload()
-}
 
-async function ver1(kernel) {
-    deleteFiles(kernel, [
-        "scripts/action/clipboard/ClearClipboard",
-        "scripts/ui/clipboard.js",
-        "scripts/ui/clipboard-data.js",
-        "scripts/ui/clipboard-search.js"
-    ])
+    #deleteFiles() {
+        this.files.forEach(file => {
+            if ($file.exists(file)) {
+                this.kernel.print(`delete file: ${file}`)
+                $file.delete(file)
+            }
+        })
+    }
 
-    rebuildDatabase(kernel, "clipboard", "clips")
+    rebuildDatabase(oldTab, newTab) {
+        this.databases.push([oldTab, newTab])
+    }
 
-    await rebuildUserActions(kernel, {
-        uncategorized: ["ExportAllContent", "DisplayClipboard"],
-        clipboard: ["B23Clean"]
-    })
+    #rebuildDatabase() {
+        const action = (oldTab, newTab) => {
+            const result = this.kernel.storage.sqlite.query(
+                `SELECT count(*), name FROM sqlite_master WHERE type = "table" AND name = "${oldTab}"`
+            )
+            if (result.error !== null) {
+                throw new Error(
+                    `Code [${result.error.code}] ${result.error.domain} ${result.error.localizedDescription}`
+                )
+            }
+            result.result.next()
+            const count = result.result.get(0)
+            result.result.close()
 
-    // 键盘高度保存到 setting
-    if ($cache.get("caio.keyboard.height")) {
-        kernel.setting.set("keyboard.previewAndHeight", $cache.get("caio.keyboard.height"))
-        $cache.remove("caio.keyboard.height")
+            if (count > 0) {
+                this.kernel.print(`copy data from old table: ${oldTab}`)
+                this.kernel.storage.sqlite.update(`INSERT INTO ${newTab} SELECT * FROM ${oldTab}`)
+                this.kernel.print(`drop table: ${oldTab}`)
+                this.kernel.storage.sqlite.update(`DROP TABLE ${oldTab}`)
+            }
+        }
+        this.databases.forEach(db => {
+            action(db[0], db[1])
+        })
+    }
+
+    rebuildUserActions(actions = {}) {
+        for (let type of Object.keys(actions)) {
+            actions[type].forEach(action => {
+                if (!this.actions[type]) {
+                    this.actions[type] = []
+                }
+                if (!this.actions[type].includes(action)) {
+                    this.actions[type].push(action)
+                }
+            })
+        }
+    }
+
+    async #rebuildUserActions() {
+        if (Object.keys(this.actions).length === 0) return
+        const actionPath = `scripts/action`
+        const userActionPath = `${this.kernel.fileStorage.basePath}/user_action`
+
+        const changeList = []
+        for (let type of Object.keys(this.actions)) {
+            this.actions[type].forEach(action => {
+                const config = JSON.parse($file.read(`${actionPath}/${type}/${action}/config.json`).string)
+                changeList.push(config.name)
+            })
+        }
+        const alertResult = await $ui.alert({
+            title: $l10n("compatibility.rebuildUserAction.alert.title"),
+            message:
+                $l10n("compatibility.rebuildUserAction.alert.message") +
+                "\n" +
+                JSON.stringify(changeList, null, 2) +
+                "\n" +
+                $l10n("compatibility.rebuildUserAction.alert.message2"),
+            actions: [{ title: $l10n("OK") }, { title: $l10n("CANCEL") }]
+        })
+        if (alertResult.index === 1) {
+            return
+        }
+
+        // 重建用户动作
+        for (let type of Object.keys(this.actions)) {
+            this.actions[type].forEach(action => {
+                if ($file.exists(`${userActionPath}/${type}/${action}`)) {
+                    this.kernel.print(`rebuild user action: ${type}/${action}`)
+                    $file.copy({
+                        src: `${actionPath}/${type}/${action}/main.js`,
+                        dst: `${userActionPath}/${type}/${action}/main.js`
+                    })
+                }
+            })
+        }
+        this.kernel.actionManager.setNeedReload()
+    }
+
+    async do() {
+        this.#deleteFiles()
+        this.#rebuildDatabase()
+        await this.#rebuildUserActions()
     }
 }
 
-async function ver2(kernel, userVersion) {
-    deleteFiles(kernel, [
-        "scripts/storage.js",
-        "scripts/ui/clips-data.js",
-        "scripts/ui/components/action-manager-data.js"
-    ])
+class VersionActions {
+    version = 6
+    userVersion = $cache.get("compatibility.version") ?? 0
 
-    rebuildDatabase(kernel, "pin", "favorite")
+    /**
+     * @param {AppKernel} kernel
+     */
+    constructor(kernel) {
+        this.kernel = kernel
+        this.compatibility = new Compatibility(this.kernel)
+    }
 
-    if (userVersion !== 1) {
-        // 用户版本为 1 的时候已经修改了 ExportAllContent
-        await rebuildUserActions(kernel, {
+    do() {
+        if (this.userVersion < this.version) {
+            this.kernel.print(`compatibility: userVersion [${this.userVersion}] lower than [${this.version}]`)
+        }
+        for (let i = this.userVersion + 1; i <= this.version; i++) {
+            this.call(i)
+        }
+        this.compatibility.do().catch(e => this.kernel.error(e))
+
+        // 修改版本
+        $cache.set("compatibility.version", this.version)
+    }
+
+    call(version) {
+        if (typeof this[`ver${version}`] === "function") {
+            this[`ver${version}`]()
+        } else {
+            throw new ReferenceError(`version ${version} undefined`)
+        }
+    }
+
+    ver1() {
+        this.compatibility.deleteFiles([
+            "scripts/action/clipboard/ClearClipboard",
+            "scripts/ui/clipboard.js",
+            "scripts/ui/clipboard-data.js",
+            "scripts/ui/clipboard-search.js"
+        ])
+
+        this.compatibility.rebuildDatabase("clipboard", "clips")
+
+        this.compatibility.rebuildUserActions({
+            uncategorized: ["ExportAllContent", "DisplayClipboard"],
+            clipboard: ["B23Clean"]
+        })
+
+        // 键盘高度保存到 setting
+        if ($cache.get("caio.keyboard.height")) {
+            this.kernel.setting.set("keyboard.previewAndHeight", $cache.get("caio.keyboard.height"))
+            $cache.remove("caio.keyboard.height")
+        }
+    }
+
+    ver2() {
+        this.compatibility.deleteFiles([
+            "scripts/storage.js",
+            "scripts/ui/clips-data.js",
+            "scripts/ui/components/action-manager-data.js"
+        ])
+
+        this.compatibility.rebuildDatabase("pin", "favorite")
+
+        this.compatibility.rebuildUserActions({
             uncategorized: ["ExportAllContent"]
         })
     }
-}
 
-async function ver3(kernel) {
-    await rebuildUserActions(kernel, {
-        clipboard: ["SendToWin"]
-    })
-}
-
-function ver4() {
-    const actionSyncDataPath = "/storage/user_action/data.json"
-    if ($file.exists(actionSyncDataPath)) {
-        const date = JSON.parse($file.read(actionSyncDataPath).string).date
-        $file.write({
-            data: $data({ string: JSON.stringify({ timestamp: date }) }),
-            path: "/storage/user_action/sync.json"
+    ver3() {
+        this.compatibility.rebuildUserActions({
+            clipboard: ["SendToWin"]
         })
-        $file.delete(actionSyncDataPath)
+    }
+
+    ver4() {
+        const actionSyncDataPath = "/storage/user_action/data.json"
+        if ($file.exists(actionSyncDataPath)) {
+            const date = JSON.parse($file.read(actionSyncDataPath).string).date
+            $file.write({
+                data: $data({ string: JSON.stringify({ timestamp: date }) }),
+                path: "/storage/user_action/sync.json"
+            })
+            $file.delete(actionSyncDataPath)
+        }
+    }
+
+    ver5() {
+        this.compatibility.rebuildUserActions({
+            uncategorized: ["DisplayClipboard"]
+        })
+    }
+
+    ver6() {
+        this.compatibility.rebuildUserActions({
+            clipboard: ["GetFromWin"]
+        })
     }
 }
 
-async function ver5(kernel) {
-    await rebuildUserActions(kernel, {
-        uncategorized: ["DisplayClipboard"]
-    })
-}
-
 /**
- *
  * @param {AppKernel} kernel
  */
 async function compatibility(kernel) {
     if (!kernel) return
 
-    const version = 5
-    const userVersion = $cache.get("compatibility.version") ?? 0
-
     try {
-        if (userVersion < 1) {
-            kernel.print(`compatibility: userVersion [${userVersion}] lower than [1], start action`)
-            await ver1(kernel)
-        }
-        if (userVersion < 2) {
-            kernel.print(`compatibility: userVersion [${userVersion}] lower than [2], start action`)
-            await ver2(kernel, userVersion)
-        }
-        if (userVersion < 3) {
-            kernel.print(`compatibility: userVersion [${userVersion}] lower than [3], start action`)
-            await ver3(kernel)
-        }
-        if (userVersion < 4) {
-            kernel.print(`compatibility: userVersion [${userVersion}] lower than [4], start action`)
-            ver4()
-        }
-        if (userVersion < 5) {
-            kernel.print(`compatibility: userVersion [${userVersion}] lower than [5], start action`)
-            ver5(kernel)
-        }
+        const versionActions = new VersionActions(kernel)
+        versionActions.do()
     } catch (error) {
         kernel.error(error)
         throw error
     }
-
-    $cache.set("compatibility.version", version) // 修改版本
 }
 
 module.exports = compatibility
