4345f9ebf7b8ec0c865ae525bee4e6ebc4881e2d
ipuppet <zimingw.ol@outlook.com>
2023-09-03 18:57:47 +0800

    优化动作

--- scripts/compatibility.js
+++ scripts/compatibility.js
@@ -129,7 +129,7 @@ class Compatibility {
 }
 
 class VersionActions {
-    version = 7
+    version = 8
     userVersion = $cache.get("compatibility.version") ?? 0
 
     /**
@@ -231,6 +231,12 @@ class VersionActions {
     ver7() {
         this.compatibility.rebuildUserActions({
             uncategorized: ["Replace"]
+        })
+    }
+
+    ver8() {
+        this.compatibility.rebuildUserActions({
+            uncategorized: ["SendToWin"]
         })
     }
 }
