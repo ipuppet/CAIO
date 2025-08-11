e46a1b584ece81730cd15dd32b242a1ca74afb5d
Samuel <zimingw.ol@outlook.com>
2025-08-12 01:06:14 +0800

    feat: add CLIP_ACTIONS category to clipboard settings and update localization

--- scripts/setting/general/clip.js
+++ scripts/setting/general/clip.js
@@ -62,7 +62,11 @@ module.exports = kernel => {
                         title: "CLIP_ACTIONS",
                         key: "clipboard.actions.category",
                         value: kernel.actions.getActionCategories()[0]
-                    }).with({ pullDown: true, items: () => kernel.actions.getActionCategories() })
+                    }).with({
+                        pullDown: true,
+                        items: () => kernel.actions.getActionCategories(),
+                        values: () => kernel.actions.getActionCategories()
+                    })
                 ]
             },
             {
